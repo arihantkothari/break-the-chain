5e759f1b791ddbfd1c1f915744bfde82f5e250dd
arihantkothari <arihantkothari17@gmail.com>
2020-03-28 20:41:40 +0530

    bluetooth feature init

--- break-the-chain/App.tsx
+++ break-the-chain/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Break The Chain</Text>
     </View>
   );
 }
@@ -12,7 +12,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#a00',
     alignItems: 'center',
     justifyContent: 'center',
   },
